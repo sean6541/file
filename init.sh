9166105fadbc759343277a08cccdf083d1eabb00
Sean Olson <seandolson654@gmail.com>
2017-06-22 12:47:12 -0700

    Delete init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-cd ~/
-wget https://raw.githubusercontent.com/sean6541/file/master/pifmrds.deb
-sudo dpkg -i ./pifmrds.deb
-wget https://raw.githubusercontent.com/sean6541/file/master/repository.foreverguest-1.1.0.zip
-wget https://raw.githubusercontent.com/sean6541/file/master/repository.kodi.sean.zip
-echo DONE
