c89f305c1ee6ee518754b9548d4218ebac32a685
Sean Olson <seandolson654@gmail.com>
2018-01-31 15:14:51 -0800

    Add files via upload

--- showfi.bat
+++ showfi.bat
@@ -1,5 +1,4 @@
 @echo off
-Setlocal EnableDelayedExpansion
 set /p ssid="Network SSID: "
 netsh wlan show profile name="%ssid%" key=clear > tmp1.out
 findstr /c:"Key Content" tmp1.out > tmp2.out
@@ -8,15 +7,17 @@ del tmp1.out
 del tmp2.out
 if NOT "%key%" == "" (
 	echo Password: %key%
-	set /p save="Save To File (Y/N)? "
-	if !save! == y set save=Y
-	if "!save!" == "Y" (
-		(
-		echo SSID: %ssid%
-		echo Password: %key%
-		)>network.txt
-	)
 ) else (
 	echo No network with the name "%ssid%" was found.
+	goto END
 )
+set /p save="Save To File (Y/N)? "
+if "%save%" == "y" set save=Y
+if "%save%" == "Y" (
+	(
+	echo SSID: %ssid%
+	echo Password: %key%
+	)>network.txt
+)
+:END
 pause
