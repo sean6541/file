c8dc5b35b2ef18e8abcf39f3162b13b6f89a43a4
Sean Olson <seandolson654@gmail.com>
2018-03-16 07:03:57 -0700

    Add files via upload

---
+++ bmvinstall.sh
@@ -0,0 +1,16 @@
+apt-get update
+apt-get install apache2 libapache2-mod-php php
+mkdir /var/www/html/VictronBMV
+cd /var/www/html/VictronBMV
+wget https://raw.githubusercontent.com/sean6541/file/master/VictronBMV.zip
+unzip -j ./VictronBMV.zip
+rm ./VictronBMV.zip
+cd ../
+chmod -R 755 ./VictronBMV
+mv ./VictronBMV/VictronBMV.conf /etc/apache2/sites-available
+a2ensite VictronBMV
+echo 'ALL ALL=NOPASSWD: /var/www/html/VictronBMV/runcat.sh' | EDITOR='tee -a' visudo
+echo '@reboot stty -F /dev/ttyS0 speed 19200 raw -echo' | EDITOR='tee -a' crontab -e
+echo 'Rebooting in 5 sec. Ctrl-C to cancel.'
+sleep 5
+reboot
