26362a12286ad9128dc5db6fe8048c3b789a5c84
Sean Olson <seandolson654@gmail.com>
2018-06-01 12:52:45 -0700

    Add files via upload

---
+++ tuyasetup.py
@@ -0,0 +1,25 @@
+import tuyaapi
+
+input('This script will assist you in setting up your Tuya device for use with tuyaapi. You must have an active internet connection and a working WiFi card on the device running this script. If these requirements are met, press enter to continue...')
+email = input('Enter your Tuya account email (if you haven\'t run this script before, your account will be created): ')
+passwd = input('Enter your Tuya account password: ')
+tapi = tuyaapi.TuyaAPI(email, passwd)
+tok = tapi.gentok()
+ssid = input('Enter the SSID of the WiFi network your device should connect to: ')
+passw = input('Enter the password of the WiFi network your device should connect to: ')
+input('Now put your Tuya device in AP setup mode by holding the power switch until blue light blinks fast. When it is blinking fast, let go, wait 3 seconds, then hold it again until the blue light blinks slowly. Once in AP mode, connect to your devices AP (usually named SmartLife_xxxx). Press enter when done...')
+tapi.setupdev(ssid, passw, tok['token'], tok['secret'])
+input('Now reconnect to the internet. Press enter when done...')
+devid = None
+done = False
+while done == False:
+    try:
+        devid = tapi.ldevbytok(tok['token'])
+        done = True
+    except IndexError:
+        done = False
+        time.sleep(1.5)
+print('Device ID: ' + devid)
+print('Username: ' + email)
+print('Password: ' + passwd)
+print('Done! Your device is now registered under the username and password above. You can now use the tuyaapi library or Home Assistant to control it.')
