3ec6abb56eff11fd29996ac15dc8d898382d8e44
Sean Olson <seandolson654@gmail.com>
2019-06-02 14:23:46 -0700

    Add files via upload

--- ppp.sh
+++ ppp.sh
@@ -2,10 +2,10 @@
 
 if (( $# == 3 )); then
     if [ "$1" = "gateway" ] || [ "$1" = "g" ]; then
-        pppd "$2" "$3" 192.168.4.1:192.168.4.2 lock passive local noauth
+        pppd "$2" "$3" 192.168.4.1:192.168.4.2 lock passive local noauth nocrtscts
         exit 0
     elif [ "$1" = "client" ] || [ "$1" = "c" ]; then
-        pppd "$2" "$3" 192.168.4.2:192.168.4.1 lock passive local noauth persist
+        pppd "$2" "$3" 192.168.4.2:192.168.4.1 lock passive local noauth persist nocrtscts
         exit 0
     fi
 fi
